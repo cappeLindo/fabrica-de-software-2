41abb90a2a1459fcf02e71ecfc04fdccf3ca2b41
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-04-17 11:20:02 -0400

    atualizar

--- TelasHtmleCSS/Hiago/codigoFinal/js/scriptCampoIpva.js
+++
@@ -1,9 +0,0 @@
-function toggleDataVencimento() {
-    var campoIpva = document.getElementById('campoIpva');
-    var campoDataVencimento = document.getElementById('campoDataVencimento');
-    if (campoIpva.checked) {
-        campoDataVencimento.style.display = 'flex'; // Exibe o campo de data se estiver marcado
-    } else {
-        campoDataVencimento.style.display = 'none';  // Esconde o campo de data se desmarcado
-    }
-}
