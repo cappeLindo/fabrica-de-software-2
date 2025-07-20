45329c9a97a5a7047b4e95b698aa35ad4bee39d7
hiagogabrielga <146982525+hiagogabrielga@users.noreply.github.com>
2025-07-19 22:03:49 -0400

    atualização

--- webcars/next.config.mjs
+++ webcars/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'localhost:3000', 'webcars.dev.vilhena.ifro.edu.br'],
   },
 };
 
