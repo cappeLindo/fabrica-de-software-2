6359d7a7cf0ae0ccf6efc241e53203c91b055d52
hiagogabrielga <146982525+hiagogabrielga@users.noreply.github.com>
2025-07-19 22:15:27 -0400

    Revert "teste"
    This reverts commit 3790bb90c2af8e69874ad2e61de852465ead14d2.

--- webcars/next.config.mjs
+++ webcars/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'localhost:3000', 'https://webcars.dev.vilhena.ifro.edu.br'],
+    domains: ['localhost', 'localhost:3000', 'webcars.dev.vilhena.ifro.edu.br'],
   },
 };
 
